042cad1e33d97ba03d3bc2c327e78f2ba303fea2
Toni R <trahyns@gmail.com>
2020-08-26 01:21:15 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,80 +1,49 @@
-$(function(params) {
-    // Set cursor on the search bar on page load
-    $('#inlineFormInputGroup').focus();
+$(document).ready(function() {
+    //wehn the from "search" is submitted, do this function 
+    $("#search").submit(searchGIFY)
+})
 
-    //Create the API URL from the user inputs, API Key and Pagination
-    var api = 'https://api.giphy.com/v1/gifs/search?q=';
-    var query = 'Chicago';
-    var apiKey = '&api_key=bJdrGmWEmdF2gIQTv3b7jNYgFmmBzDEV';
-    var lang = '&lang=es';
-    var apiPagination = 15; //Default Pagination
-    var flag = 0; // for normal search
 
-    $("#submit").click(function() {
+function searchGIFY(e) {
+    // prevents the original function of submit and doesnt reload page.
+    e.preventDefault();
 
-        query = $('#inlineFormInputGroup').val();
+    //url must have http or https when using chrome or will get CORS error
+    var url = 'https://api.giphy.com/v1/gifs/search';
+    //make everything lowercase incase api doesnt accept capital letters
+    var query = $("#inlineFormInputGroup").val().toLowerCase();
+    var apiKey = 'HwjxeK4xnDZ6kp3yqqzoUusAfJR3RmHk';
 
-        //Display error if no input entered
-        if ((query.length == 0) && (flag == 0)) {
-            $(".form-inline").effect("shake");
-            $("#inlineFormInputGroup").addClass("error");
-        } else {
-            $("#inlineFormInputGroup").removeClass("error");
+    //api key must go first for authorization to api
+    $.ajax({
+        url: url,
+        dataType: 'JSON',
+        data: {
+            'api_key': apiKey,
+            'q': query
+        },
+        success: function(result) {
+            console.log(result);
+            for (var i = 0; i < result.length; i++) {
 
-            //Generate entire URL from the user input
-            var url = api + query + apiKey + lang + '&limit=' + apiPagination;
-            var fetchData = $.getJSON(url, gotData);
+                var showDiv = $("<div class='gif-results'>");
 
-        } // end check if valid input
-    }); //end submit click event
+                var rating = result[i].rating;
+                var defaultAnimatedSrc = result[i].images.fixed_height.url;
+                var staticSrc = result[i].images.fixed_height_still.url;
+                var showImage = $("<img>");
+                var p = $("<p>").text("Rating: " + rating);
+                showImage.addClass("gif");
+                showImage.attr("src", staticSrc);
+                showImage.attr("data-state", "still");
+                showImage.attr("data-still", staticSrc);
+                showImage.attr("data-animate", defaultAnimatedSrc);
+                showDiv.append(p);
+                showDiv.append(showImage);
+                $("#getdata").prepend(showDiv);
 
-}); //document ready function end
-
-function gotData(data) {
-    //continue if API exists
-    if (data.meta.status) {
-        if (data.data.length > 0) {
-            $(".gif-results").show();
-            $('.no-result').hide();
-
-            //Remove any existing data - used to clearing out previous searches
-            if ($("div.gif-results img").length > 0) {
-                $("div.gif-results img").remove();
             }
-
-            $('div.gif-results').addClass("well");
-            $('.loading-gif').css("display", "block");
-
-            //Iterate through all of the returned JSON data and append to HTML 
-            for (var i = 0; i < data.data.length; i++) {
-                //Get the URL from JSON
-                var gifURL = data.data[i].images.original.url;
-                // var giphyURL = data.data[i].url;
-
-                // $(".insert-gif").attr('src', gifURL);
-                var img = $('<img class="gifs">');
-                img.attr('src', gifURL);
-                img.appendTo('.gif-results');
-
-            } //end for loop
-            $('.loading-gif').css("display", "none");
-
-            $("#load-more").show(); //show load more button at the end of the list
-            $(".default-gif").hide(); //Hide Placeholder GIF when searched
-
-        } else {
-            $(".gif-results").hide();
-            $("#load-more").hide();
-            $('.no-result').show();
         }
-
-    } //end data.meta.status 
-
-} // end function gotData
-
-
-$("form").submit(function(e) {
-    $('#submit').click();
-    //Blur out of the search bar after enter key hit
-    $('#inlineFormInputGroup').blur();
-});
+    });
+}
+//Create the API URL from the user inputs, API Key and Pagination
