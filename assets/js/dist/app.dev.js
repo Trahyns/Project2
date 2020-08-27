8b4a03804cc6cf5c54c845c10678f6327beab51f
Toni R <trahyns@gmail.com>
2020-08-26 22:13:57 -0400

    deleted original js file and created a new one

---
+++ assets/js/dist/app.dev.js
@@ -0,0 +1,30 @@
+"use strict";
+
+$(document).ready(function () {
+  function displayGifs(e) {
+    e.preventDefault();
+    var x = $("#inlineFormInputGroup").val().toLowerCase();
+    console.log(x);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?" + "&api_key=bJdrGmWEmdF2gIQTv3b7jNYgFmmBzDEV&q=" + x;
+    console.log(queryURL);
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function (response) {
+      var results = response.data;
+      console.log(results);
+
+      for (var i = 0; i < results.length; i++) {
+        //Get the URL from JSON
+        var gifURL = results[i].images.original.url; // var giphyURL = results[i].url;
+        // $(".insert-gif").attr('src', gifURL);
+
+        var img = $('<img class="gifs">');
+        img.attr('src', gifURL);
+        img.appendTo('.gif-results');
+      }
+    });
+  }
+
+  $("#submit").click(displayGifs);
+});
